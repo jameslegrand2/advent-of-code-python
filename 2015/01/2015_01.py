4a07e7a3e2fba66292cb0ddfbda402ec4c158f5e
jameslegrand2 <jameslegrand2@gmail.com>
2022-01-20 17:57:22 -0600

    day 1 complete

--- 2015/01/2015_01.py
+++ 2015/01/2015_01.py
@@ -1,9 +1,31 @@
+from collections import Counter
+
 try:
-    input = open('Input.txt','r')
-    str = input.read()
-    input.close
+    inputfile = open('.\\2015\\01\\Input.txt','r')
+    inputdata = inputfile.read()
+    inputfile.close
 except:
     print("Could not obtain input from file!")
 
-print(str)
+#Part 1
+counts = Counter(inputdata)
+total1 = counts['('] - counts[')']
 
+#Part 2
+position = 0
+total2 = 0
+for character in inputdata:
+    position += 1
+    if character == '(':
+        total2 += 1
+    elif character == ')':
+        total2 -=1
+    if total2 < 0:
+        break
+        
+try:
+    output = open('.\\2015\\01\\Output.txt','w')
+    output.write(f"{total1}\n{position}")
+    output.close()
+except:
+    print("Could not write output file!")
